569f00a4f1e012d1d6f56c11f49385a53b91283c
Aman Desai <amandesai01@gmail.com>
2020-12-07 19:04:07 +0530

    entity: Note

---
+++ src/main/java/com/journaldigs/api/models/Note.java
@@ -0,0 +1,58 @@
+package main.java.com.journaldigs.api.models;
+
+import java.util.Date;
+import java.util.UUID;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Note {
+    @Id
+    private String noteid;
+    private String title;
+    private String contents;
+    private Date createdDate;
+
+    @ManyToOne
+    private Journal journal; 
+
+    public Note(String title, String contents, Date createdDate, Journal j){
+        this.id = UUID.randomUUID();
+        this.title = title;
+        this.contents = contents;
+        this.createdDate = createdDate;
+        this.journal = j;
+    }
+
+    public String getNoteid() {
+        return this.noteid;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContents() {
+        return this.contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public Date getCreatedDate() {
+        return this.createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    
+}
