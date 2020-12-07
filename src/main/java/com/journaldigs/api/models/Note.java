d68b6d717a1a9f91c46ac01f6c861e3a16b7e93d
Aman Desai <amandesai01@gmail.com>
2020-12-07 19:38:29 +0530

    entity: bug fixes

--- src/main/java/com/journaldigs/api/models/Note.java
+++ src/main/java/com/journaldigs/api/models/Note.java
@@ -1,4 +1,4 @@
-package main.java.com.journaldigs.api.models;
+package com.journaldigs.api.models;
 
 import java.util.Date;
 import java.util.UUID;
--- src/main/java/com/journaldigs/api/models/User.java
+++ src/main/java/com/journaldigs/api/models/User.java
@@ -1,4 +1,4 @@
-package main.java.com.journaldigs.api.models;
+package com.journaldigs.api.models;
 
 import java.util.UUID;
 
