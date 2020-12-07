636a96ace5314b7a7c05f4cb46c7508f87d66f60
Aman Desai <amandesai01@gmail.com>
2020-12-07 19:04:34 +0530

    entity: User

---
+++ src/main/java/com/journaldigs/api/models/User.java
@@ -0,0 +1,51 @@
+package main.java.com.journaldigs.api.models;
+
+import java.util.UUID;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User{
+    @Id
+    private String id;
+    private String name;
+    private String phoneno;
+    private String emailid;
+
+    public User(String name, String phoneno, String emailid){
+        this.id = UUID.randomUUID();
+        this.name = name;
+        this.phoneno = phoneno;
+        this.emailid = emailid;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneno() {
+        return this.phoneno;
+    }
+
+    public void setPhoneno(String phoneno) {
+        this.phoneno = phoneno;
+    }
+
+    public String getEmailid() {
+        return this.emailid;
+    }
+
+    public void setEmailid(String emailid) {
+        this.emailid = emailid;
+    }
+
+}
