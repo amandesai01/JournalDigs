eb64f24a78daa5a5db25a4a0daa302be15c96f90
Aman Desai <amandesai01@gmail.com>
2020-12-07 21:44:26 +0530

    db: user: getuserbyemail added

--- src/main/java/com/journaldigs/api/databases/UserDB.java
+++ src/main/java/com/journaldigs/api/databases/UserDB.java
@@ -4,4 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.journaldigs.api.models.User;
 
-public interface UserDB extends CrudRepository<User, String> {}
+public interface UserDB extends CrudRepository<User, String> {
+    public User getUserByEmail(String email);
+}
