b64f1e525b13f4dc4d6e69e5c7d63e10aaa711a3
Aman Desai <amandesai01@gmail.com>
2020-12-07 23:55:58 +0530

    service: refactor: imported logic to service layer

--- src/main/java/com/journaldigs/api/services/UserService.java
+++ src/main/java/com/journaldigs/api/services/UserService.java
@@ -1,5 +1,8 @@
 package com.journaldigs.api.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.journaldigs.api.databases.UserDB;
 import com.journaldigs.api.models.User;
 
@@ -11,11 +14,14 @@ public class UserService {
     @Autowired
     private UserDB userDB;
 
+    @Autowired
+    private JwtService jwtService;
+
     public User getUserById(String userid){
         return userDB.findById(userid).get();
     }
 
-    public String insertNewUser(String name, String phoneno, String emailid, String password){
+    private String insertNewUser(String name, String phoneno, String emailid, String password){
         User user = new User(name, phoneno, emailid, password);
         String id = user.getId();
         userDB.save(user);
@@ -24,5 +30,32 @@ public class UserService {
 
     public User getUserByEmail(String email){
         return userDB.getUserByEmail(email);
+    }
+
+    public Map<String, String> Signup(String name, String phoneno, String email, String password){
+        Map<String, String> response = new HashMap<>();
+        try {
+            String id = this.insertNewUser(name, phoneno, email, password);
+            String token = this.jwtService.getTokenForUser(id);
+            response.put("status", "OK");
+            response.put("token", token);
+            return response;
+        } catch (Exception e) {
+            response.put("status", "FAIL");
+            response.put("error", e.toString());
+            return response;
+        }
+    }
+
+    public Map<String, String> Login(String email, String password){
+        Map<String, String> response = new HashMap<>();
+        User user = this.getUserByEmail(email);
+        if(user != null &&  user.getPassword().equals(password)){
+            response.put("status", "OK");
+            response.put("token", this.jwtService.getTokenForUser(user.getId()));
+            return response;
+        }
+        response.put("status", "FAIL");
+        return response;
     }
 }
