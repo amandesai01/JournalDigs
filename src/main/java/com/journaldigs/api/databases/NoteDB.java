d30c38398c7eff99ae8065965c4f0c062b059e7f
Aman Desai <amandesai01@gmail.com>
2020-12-08 15:00:59 +0530

    db: notes: new crud

--- src/main/java/com/journaldigs/api/databases/NoteDB.java
+++ src/main/java/com/journaldigs/api/databases/NoteDB.java
@@ -2,6 +2,11 @@ package com.journaldigs.api.databases;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
+import com.journaldigs.api.models.Journal;
 import com.journaldigs.api.models.Note;
 
-public interface NoteDB extends CrudRepository<Note, String> {}
+public interface NoteDB extends CrudRepository<Note, String> {
+    public List<Note> findByJournal(Journal journal);
+}
